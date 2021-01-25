b07444939d64fe7f90625b548daf23b7200fb09a
blupine <skwint11@gmail.com>
2021-01-25 20:38:46 +0900

    Add Account entity class

---
+++ src/main/java/com/studyolleh/domain/Account.java
@@ -0,0 +1,44 @@
+package com.studyolleh.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Account {
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String passwd;
+
+    private boolean emailVerified;
+
+    private String emailCheckToken;
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    private boolean studyCreatedByEmail;
+
+    private boolean studyCreatedByWeb;
+
+    private boolean studyEnrollmentResultByEmail;
+
+    private boolean studyEnrollmentResultByWeb;
+
+    private boolean studyUpdatedByEmail;
+
+    private boolean studyUpdatedByWeb;
+}
