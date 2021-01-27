7769c7b87128a71e53490cd2fb07605615d94599
blupine <skwint11@gmail.com>
2021-01-27 19:54:50 +0900

    Add logout and UserDetailService implementation

--- src/main/java/com/studyolleh/account/AccountService.java
+++ src/main/java/com/studyolleh/account/AccountService.java
@@ -7,6 +7,9 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,7 +19,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class AccountService {
+public class AccountService implements UserDetailsService {
 
     private final AccountRepository accountRepository;
     private final JavaMailSender javaMailSender;
@@ -57,5 +60,17 @@ public class AccountService {
                 account.getPassword(),
                 List.of(new SimpleGrantedAuthority("ROLE_USER")));
         SecurityContextHolder.getContext().setAuthentication(token);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String eamilOrNickname) throws UsernameNotFoundException {
+        Account account = accountRepository.findByEmail(eamilOrNickname);
+        if(account == null){
+            account = accountRepository.findByNickname(eamilOrNickname);
+        }
+        if(account == null){
+            return null;
+        }
+        return new UserAccount(account);
     }
 }
