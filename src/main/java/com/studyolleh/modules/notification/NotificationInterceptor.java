f078fb54529c1702a85fcab168d19a1c4c60a4e6
blupine <skwint11@gmail.com>
2021-02-12 15:37:19 +0900

    Add notification icon using HandlerInterceptor

---
+++ src/main/java/com/studyolleh/modules/notification/NotificationInterceptor.java
@@ -0,0 +1,43 @@
+package com.studyolleh.modules.notification;
+
+import com.studyolleh.modules.account.Account;
+import com.studyolleh.modules.account.UserAccount;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+@RequiredArgsConstructor
+public class NotificationInterceptor implements HandlerInterceptor {
+
+    private final NotificationRepository notificationRepository;
+
+    /**
+     * View 렌더링 이전에 사용자에게 Notifiaction이 있는지 여부 확인 후 모델에 넘겨줌
+     * @param request
+     * @param response
+     * @param handler
+     * @param modelAndView
+     * @throws Exception
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (modelAndView != null && isRedirectView(modelAndView) && authentication != null && authentication.getPrincipal() instanceof UserAccount) {
+            Account account = ((UserAccount) authentication.getPrincipal()).getAccount();
+            Long count = notificationRepository.countByAccountAndChecked(account, false);
+            modelAndView.addObject("hasNotification", count > 0);
+        }
+    }
+
+    private boolean isRedirectView(ModelAndView modelAndView) {
+        return modelAndView.getViewName().startsWith("redirect:") || modelAndView.getView() instanceof RedirectView;
+    }
+}
--- src/main/java/com/studyolleh/modules/notification/NotificationRepository.java
+++ src/main/java/com/studyolleh/modules/notification/NotificationRepository.java
@@ -1,6 +1,8 @@
 package com.studyolleh.modules.notification;
 
+import com.studyolleh.modules.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    Long countByAccountAndChecked(Account account, boolean checked);
 }
